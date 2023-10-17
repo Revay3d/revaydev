316ea43c0c401bb40193cda6ea3696c2c204ad71
RevayDev <revaydev17@gmail.com>
2023-10-17 15:50:09 -0500

    He actualizado el codigo, la ortografia y pequeños errores visuales ya esta listo la web :)

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ menuToggle.addEventListener("click", () => {
 menuItems.classList.toggle("nav__menu--visible")
 
 });
+
+//esto fue hecho con chatgpt :( 
+// JavaScript puro
+document.querySelectorAll('.nav__item').forEach(item => {
+    item.addEventListener('click', event => {
+      // Elimina la clase 'active' de todos los elementos
+      document.querySelectorAll('.nav__item').forEach(item => {
+        item.classList.remove('active');
+      });
+      // Añade la clase 'active' al elemento que se ha hecho clic
+      event.target.classList.add('active');
+    });
+  });
+  
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});  
+
