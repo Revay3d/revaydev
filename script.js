1f76e04d5395c09c560b4ef3f24347944e966fde
RevayDev <revaydev17@gmail.com>
2023-12-21 18:42:40 -0500

    le puse un loader y animaciones para que se vea muy bonito mas adelante optimizare el codigo y organisarlo

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
-const menuToggle = document.querySelector(".menu-toggle")
-const menuItems = document.querySelector(".nav__items")
+const menuToggle = document.querySelector(".menu-toggle");
+const menuItems = document.querySelector(".nav__items");
 
 menuToggle.addEventListener("click", () => {
-
-menuItems.classList.toggle("nav__menu--visible")
-
+  menuItems.classList.toggle("nav__menu--visible");
 });
 
-//esto fue hecho con chatgpt :( 
+//esto fue hecho con chatgpt :(
 // JavaScript puro
-document.querySelectorAll('.nav__item').forEach(item => {
-    item.addEventListener('click', event => {
-      // Elimina la clase 'active' de todos los elementos
-      document.querySelectorAll('.nav__item').forEach(item => {
-        item.classList.remove('active');
-      });
-      // Añade la clase 'active' al elemento que se ha hecho clic
-      event.target.classList.add('active');
+document.querySelectorAll(".nav__item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    // Elimina la clase 'active' de todos los elementos
+    document.querySelectorAll(".nav__item").forEach((item) => {
+      item.classList.remove("active");
+    });
+    // Añade la clase 'active' al elemento que se ha hecho clic
+    event.target.classList.add("active");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
+});
+
+const elemento = document.querySelectorAll(".elemento");
+
+const cargarImagen = (entrada, observador) => {
+  entrada.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("visible");
+    } else {
+      entrada.target.classList.remove("visible");
+    }
+  });
+};
+
+const observador = new IntersectionObserver(cargarImagen, {
+  root: null,
+  rootMargin: "1px 0px",
+  threshold: 0.20,
+});
+
+elemento.forEach((elemento) => observador.observe(elemento));
+
+
+window.addEventListener('load', function() {
   
+  setTimeout(function() {
+    var loader = document.querySelector("body");
+    loader.classList.add('loader');
+  
+  }, 500);
 
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+   
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});  
 
+});
