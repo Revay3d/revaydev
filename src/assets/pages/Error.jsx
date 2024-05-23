d3f4b739d4b28fd6fcc9db668b664389d772619f
RevayDev <revaydev17@gmail.com>
2024-05-23 18:47:08 -0500

    arregle el error 404 y estoy haciendo una prueba

--- src/assets/pages/Error.jsx
+++ src/assets/pages/Error.jsx
@@ -4,11 +4,13 @@ import Flash from '../components/Flash';
 
 export default function Error() {
   return (
+    <div className='caja__error'>
     <div className='error' >
         <Flash Tipo="error"/>
       <p className='error__mensaje'>404</p>
       <p className='error__descripcion'>Upps... Parese que este sitio no esta disponibles o no exite.</p>
       < NavHashLink to="/#" >  <button className='error__botom'>Volver al inicio</button> </NavHashLink>
+    </div>
     </div>
   )
 }
